9117a846b79e8394fbccf92eaa1462e6c28098f9
zhangjiarui <zhangjiarui77@dingtalk.com>
2026-06-22 23:08:37 +0800

    011

--- 备份/011_toRefs与toRef/components/Person.vue
+++ 备份/011_toRefs与toRef/components/Person.vue
@@ -8,14 +8,19 @@
 </template>
 
 <script lang="ts" setup name="Person">
-  import {reactive,toRefs,toRef} from 'vue'
+  import {reactive,toRefs,toRef,ref} from 'vue'
 
   // 数据
+  // let person_test = ref({
+  //   name: '张三',
+  //   age: 18
+  // })
   let person = reactive({
     name:'张三',
     age:18
   })
 
+  // toRefs：将响应式对象中的属性解构出来，且这些属性依然是响应式的，每个属性都是ref类型
   // 使用toRefs从person这个响应式对象中，解构出name、age，且name和age依然是响应式的
   // name和age的值是ref类型，其value值指向的是person.name和person.age
   let {name,age} = toRefs(person)
