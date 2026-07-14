c784d93b77039c60c4e744dad7f0973369af7ff8
zhangjiarui <zhangjiarui77@dingtalk.com>
2026-07-14 11:56:21 +0800

    033

--- 备份/033_路由_replace属性/App.vue
+++ 备份/033_路由_replace属性/App.vue
@@ -3,6 +3,13 @@
     <Header/>
     <!-- 导航区 -->
     <div class="navigate">
+      <!--
+      Vue Router 提供两种把新页面加进历史记录的方式：replace和push，
+                                                  replace不会留下历史记录，push会留下历史记录 
+      假设访问顺序：A → B → C ：push 模式，历史栈 = [A, B, C]，在 C 页面点后退 → 回到 B
+      加了 replace（如从 B 跳 C 时用 replace）：历史栈 = [A, C]，在 C 页面点后退 → 直接回到 A（B 被"顶掉"了）
+      -->
+      */
       <RouterLink replace to="/home" active-class="active">首页</RouterLink>
       <RouterLink replace :to="{name:'xinwen'}" active-class="active">新闻</RouterLink>
       <RouterLink replace :to="{path:'/about'}" active-class="active">关于</RouterLink>
